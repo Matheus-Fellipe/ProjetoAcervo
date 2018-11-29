17f7bc6a440cb45344dd394f06b7103c5444529a
Matheus Fellipe de Oliveira Cardoso <37333557+Matheus-Fellipe@users.noreply.github.com>
2018-11-28 23:40:00 -0200

    Add files via upload

---
+++ ProjetoAcervo/ProjetoAcervo/Models/Emprestimo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAcervo.Models
+{
+    class Emprestimo
+    {
+        private DateTime dtEmprestimo;
+        private DateTime dtDevolucao;
+
+
+        public Emprestimo(DateTime dtEmp) {
+            this.dtEmprestimo = dtEmp;
+        }
+
+        public DateTime DtEmprestimo
+        {
+            get { return this.dtEmprestimo; } 
+            set { this.dtEmprestimo = value; }
+        }
+
+        public DateTime DtDevolucao
+        {
+            get { return this.dtDevolucao; }
+            set { this.dtDevolucao = value; }
+        }
+    }
+}
---
+++ ProjetoAcervo/README.md
@@ -0,0 +1,2 @@
+"# ProjetoAcervo" 
+"# ProjetoAcervo" 
